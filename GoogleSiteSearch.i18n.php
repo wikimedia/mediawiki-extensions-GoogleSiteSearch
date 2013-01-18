f093f4c4c7304c86923acb39a9973b4eb79f4b84
Ryan Finnie <ryan@finnie.org>
2013-01-18 15:18:00 -0800

    Initial import

---
+++ GoogleSiteSearch.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for GoogleSiteSearch
+ *
+ * @file
+ * @ingroup Extensions
+ */
+$messages = array();
+
+/** English (English) */
+$messages['en'] = array(
+	'googlesitesearch' => 'GoogleSiteSearch',
+	'googlesitesearch-desc' => 'Adds Google CSE site search to wiki search results',
+	'googlesitesearch-loading' => 'Loading',
+	'googlesitesearch-google-results' => 'Google site results',
+	'googlesitesearch-wiki-results' => 'Wiki results',
+);
