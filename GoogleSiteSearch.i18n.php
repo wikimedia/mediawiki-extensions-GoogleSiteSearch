409183e1be83b0c9cdb0cca8a03136e7e206e26e
Ryan Finnie <ryan@finnie.org>
2013-02-24 12:46:26 -0800

    Add i18n message documentation
    Per https://www.mediawiki.org/wiki/I18n#Message_documentation
    
    Change-Id: I6d141112e02e6e84625b7740e241d6797ba5da67

--- GoogleSiteSearch.i18n.php
+++ GoogleSiteSearch.i18n.php
@@ -9,9 +9,16 @@ $messages = array();
 
 /** English (English) */
 $messages['en'] = array(
-	'googlesitesearch' => 'GoogleSiteSearch',
 	'googlesitesearch-desc' => 'Adds [//www.google.com/cse/manage/all Google CSE site search] to wiki search results',
-	'googlesitesearch-loading' => 'Loading',
+	'googlesitesearch-loading' => 'Loading...',
 	'googlesitesearch-google-results' => 'Google site results',
 	'googlesitesearch-wiki-results' => 'Wiki results',
+);
+
+/** Message documentation (Message documentation) */
+$messages['qqq'] = array(
+	'googlesitesearch-desc' => 'Special:Version description of the extension.',
+	'googlesitesearch-loading' => 'Displayed before the Google CSE JavaScript replaces it with the actual search results.  Wiki markup is not allowed here; message will be converted to escaped HTML.',
+	'googlesitesearch-google-results' => 'The title sub-header for the Google CSE results section.',
+	'googlesitesearch-wiki-results' => 'The title sub-header for the MediaWiki search results section.',
 );
