f9883c4e0a457dc457c93f967240c44028f72d70
zoranzoki21 <zorandori4444@gmail.com>
2019-12-01 16:50:18 +0100

    Remove old-backwards compatibility for GoogleSiteSearch extension
    Extension is converted to extension registration and requires MediaWiki >+1.32.0
    
    Change-Id: Ica0d1d8d44c44be95cf4e8bac5e4b2ed56bd94c0

--- GoogleSiteSearch.php
+++ GoogleSiteSearch.php
@@ -1,8 +1,4 @@
 <?php
-/** \file
-* \brief Contains setup code for the GoogleSiteSearch Extension.
-*/
-
 /**
  * GoogleSiteSearch Extension for MediaWiki
  *
@@ -34,36 +30,6 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
 	return;
+} else {
+	die( 'This version of the GoogleSiteSearch extension requires MediaWiki 1.32+' );
 }
-
-$extensionJsonFilename = dirname( __FILE__ ) . '/extension.json';
-$extensionJsonData = FormatJson::decode( file_get_contents( $extensionJsonFilename ), true );
-$wgExtensionCredits[$extensionJsonData['type']][] = array(
-	'path' => __FILE__,
-	'name' => $extensionJsonData['name'],
-	'author' => $extensionJsonData['author'],
-	'url' => $extensionJsonData['url'],
-	'descriptionmsg' => $extensionJsonData['descriptionmsg'],
-	'version' => $extensionJsonData['version'],
-	'license-name' => $extensionJsonData['license-name'],
-);
-
-# Default configuration globals
-if ( !isset( $wgGoogleSiteSearchCSEID ) ) {
-	$wgGoogleSiteSearchCSEID = '';
-}
-if ( !isset( $wgGoogleSiteSearchOnly ) ) {
-	$wgGoogleSiteSearchOnly = false;
-}
-if ( !isset( $wgGoogleSiteSearchAttributes ) ) {
-	$wgGoogleSiteSearchAttributes = [];
-}
-
-# Define internationalizations
-$wgMessagesDirs['GoogleSiteSearch'] = __DIR__ . '/i18n';
-
-# Define extension hooks
-$wgAutoloadClasses['GoogleSiteSearch'] = __DIR__ . '/GoogleSiteSearch.hooks.php';
-
-# Hook into SpecialSearchResultsPrepend
-$wgHooks['SpecialSearchResultsPrepend'][] = 'GoogleSiteSearch::searchPrepend';
